974918ac29df4c137f5ef4f02d984e3077cc5344
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-02 15:45:36 +0430

    randomized quic sort added

--- series_2/Dior/dior.py
+++ series_2/Dior/dior.py
@@ -1,5 +1,42 @@
+from random import randint
 from sys import setrecursionlimit
 setrecursionlimit(2500*1000)
+
+
+
+
+
+
+
+def partition(array: list, p: int, r: int) -> int:
+    
+    x = array[r]
+    i = p-1
+    
+    for j in range(p, r):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[r] = array[r], array[i+1]
+
+    return i+1
+
+
+def randomized_partition(array: list, p: int, r: int) -> int:
+
+    i = randint(p, r)
+    array[i], array[r] = array[r], array[i]
+
+    return partition(array, p, r)
+
+
+def randomized_quick_sort(array: list, p: int, r: int) -> None:
+
+    if p < r:
+        q = randomized_partition(array, p, r)
+        randomized_quick_sort(array, p, q-1)
+        randomized_quick_sort(array, q+1, r)
 
 
 
@@ -12,4 +49,3 @@ setrecursionlimit(2500*1000)
 
 if __name__ == "__main__":
 
-    
