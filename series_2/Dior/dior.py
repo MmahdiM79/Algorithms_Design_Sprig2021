622349278de0556a57e48c4d032adf476ef19936
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-02 17:27:09 +0430

    randomized_quick_sort function removed

--- series_2/Dior/dior.py
+++ series_2/Dior/dior.py
@@ -1,42 +1,6 @@
-from random import randint
 from sys import setrecursionlimit
 setrecursionlimit(2500*1000)
 
-
-
-
-
-
-
-def partition(array: list, p: int, r: int) -> int:
-    
-    x = array[r]
-    i = p-1
-    
-    for j in range(p, r):
-        if array[j] <= x:
-            i += 1
-            array[i], array[j] = array[j], array[i]
-
-    array[i+1], array[r] = array[r], array[i+1]
-
-    return i+1
-
-
-def randomized_partition(array: list, p: int, r: int) -> int:
-
-    i = randint(p, r)
-    array[i], array[r] = array[r], array[i]
-
-    return partition(array, p, r)
-
-
-def randomized_quick_sort(array: list, p: int, r: int) -> None:
-
-    if p < r:
-        q = randomized_partition(array, p, r)
-        randomized_quick_sort(array, p, q-1)
-        randomized_quick_sort(array, q+1, r)
 
 
 
@@ -57,17 +21,15 @@ def solve(p: int, r: int) -> None:
 
 
 
-
-
-
 if __name__ == "__main__":
 
     n = int(input())
     arr = [int(num) for num in input().split(' ')]
     pivots = [int(num) for num in input().split(' ')]
 
+    arr.sort()
 
-    randomized_quick_sort(arr, 0, n-1)
+
     places = {}
     for i in range(n):
         places[arr[i]] = i
