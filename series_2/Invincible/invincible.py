d8f8211abb58768765e824922b2af7280848a964
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-02 22:19:03 +0430

    buckets dict added

--- series_2/Invincible/invincible.py
+++ series_2/Invincible/invincible.py
@@ -11,4 +11,15 @@ if __name__ == "__main__":
 
     n, m, k = [int(num) for num in input().split(' ')]
 
+
+    buckets = {}
+    for i in range(m//k):
+        buckets[i] = []
+
+
+
+
+
     arr = [int(num) for num in input().split(' ')]
+
+
