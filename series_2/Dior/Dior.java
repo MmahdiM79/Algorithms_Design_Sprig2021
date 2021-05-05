24ca0da3e1401656efd5c150fddd501ed0f7ff66
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-05 22:46:19 +0430

    nums of array indexeis hashmap added

--- series_2/Dior/Dior.java
+++ series_2/Dior/Dior.java
@@ -8,17 +8,25 @@ import java.util.*;
 public class Dior
 {
     private static Scanner in = new Scanner(System.in);
+    private static long count = 0;
+
+
 
     public static void main(String[] args) 
     {
         int n = in.nextInt();
 
-        ArrayList<Long> arr = new ArrayList();
+        ArrayList<Long> arr = new ArrayList<>();
+        HashMap<Long, Integer> places = new HashMap<>();
+        long hold;
         for (int i = 0; i < n; i++)
-            arr.add(in.nextLong());
-
+        {
+            hold = in.nextLong();
+            arr.add(hold);
+            places.put(hold, i);
+        }
 
         Collections.sort(arr);
-        System.out.println(arr);
+        
     }
 }
