331f182b06e0cca463f990b86dc760adfc241a63
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-05 22:24:35 +0430

    reading inputs part added

--- series_2/Dior/Dior.java
+++ series_2/Dior/Dior.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner
 
 
 
@@ -7,8 +7,17 @@
 
 public class Dior
 {
+    private static Scanner in = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        
+        long n = in.nextInt();
+
+        long arr[n] = new long[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = in.nextInt();
+
+        long pivots = new long[n];
+        for (int i = 0; i < n; i++)
+            pivots[i] = in.nextInt();
     }
 }
