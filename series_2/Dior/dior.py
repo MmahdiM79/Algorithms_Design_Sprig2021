6a8bd5e908f3c6fa38cb1780f22a2c92a831615b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-05 21:55:09 +0430

    randomized quick sort function added

--- series_2/Dior/dior.py
+++ series_2/Dior/dior.py
@@ -1,6 +1,50 @@
 import sys
 sys.setrecursionlimit(2500*1000)
 
+from random import randint
+
+
+
+
+
+def partition(array: list, p: int, r: int) -> int:
+    
+    x = array[r]
+    i = p-1
+    
+    for j in range(p, r):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[r] = array[r], array[i+1]
+
+    return i+1
+
+
+
+
+def randomized_partition(array: list, p: int, r: int) -> int:
+
+    i = randint(p, r)
+    array[i], array[r] = array[r], array[i]
+
+    return partition(array, p, r)
+
+
+
+
+
+def randomized_quick_sort(array: list, p: int, r: int) -> None:
+
+    if p < r:
+        q = randomized_partition(array, p, r)
+        randomized_quick_sort(array, p, q-1)
+        randomized_quick_sort(array, q+1, r)
+
+
+
+
 
 
 
@@ -28,7 +72,7 @@ if __name__ == "__main__":
     pivots = [int(num) for num in input().split()]
 
 
-    arr.sort()
+    randomized_quick_sort(arr, 0, len(arr)-1)
 
 
     places = {}
