39753319665c506962bef9f1ee0c5e9ef6d4f7bf
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-05 23:15:40 +0430

    solve fucntion added

--- series_2/Dior/Dior.java
+++ series_2/Dior/Dior.java
@@ -8,16 +8,34 @@ import java.util.*;
 public class Dior
 {
     private static Scanner in = new Scanner(System.in);
+    private static ArrayList<Long> arr = new ArrayList<>();
+    private static long[] pivots;
+    private static int placesIndex = -1;
+    private static HashMap<Long, Integer> places = new HashMap<>();
     private static long count = 0;
 
+
+
+    private static void solve(int p, int r)
+    {
+        if (r-p >= 1)
+        {
+            placesIndex++;
+        }
+        if (r-p <= 1)
+            return;
+
+        count += r - p - 1;
+        int q = places.get(pivots[placesIndex]);
+        solve(p, q);
+        solve(q+1, r);
+    }
 
 
     public static void main(String[] args) 
     {
         int n = in.nextInt();
 
-        ArrayList<Long> arr = new ArrayList<>();
-        HashMap<Long, Integer> places = new HashMap<>();
         long hold;
         for (int i = 0; i < n; i++)
         {
@@ -26,7 +44,16 @@ public class Dior
             places.put(hold, i);
         }
 
+        pivots = new long[n];
+        for (int i = 0; i < n; i++)
+            pivots[i] = in.nextLong();
+
+
+
         Collections.sort(arr);
+
+        solve(0, n);
         
+        System.out.println(count);
     }
 }
